39af19e76f35736e7c3ebaa7684727cecc9d8c04
Daniel Grujic <DanielGT22@github.com>
2023-11-29 15:47:02 +0100

    first commit

---
+++ src/Esercizio1/Rettangolo.java
@@ -0,0 +1,21 @@
+package Esercizio1;
+
+public class Rettangolo {
+
+    private double altezza;
+    private double larghezza;
+
+   
+    public double calcolaArea() {
+        return altezza * larghezza;
+    }
+
+
+    public double calcolaPerimetro() {
+        return 2 * (altezza + larghezza);
+    }
+
+
+
+
+}
